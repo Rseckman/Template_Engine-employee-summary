76ec9705f5257d6bccfb148f9ad69ae228a0e7d9
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-09-29 09:04:48 -0400

    working program, updated html template and finished readme

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
+employees = [];
 
 // inquirer Prompt #1 Manager
 inquirer
@@ -52,18 +49,16 @@ inquirer
 
 // recursion function to create employees
 const makeEmployees = () => {
-    //inquire prompt #2 to choose what type of employee
+  //inquire prompt #2 to choose what type of employee
   inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "type of employeee",
-        message: "which type of team member would you like to add?",
-        choices: ["engineer", "intern", "no more team members"],
-      },
-    ])
+    .prompt({
+      type: "list",
+      name: "employee",
+      message: "which type of team member would you like to add?",
+      choices: ["Engineer", "Intern", "no more team members"],
+    })
     .then(function (typeAnswers) {
-      if (typeAnswers === "Engineer") {
+      if (typeAnswers.employee === "Engineer") {
         //   inquirer prompt #3 Engineer
         inquirer
           .prompt([
@@ -84,7 +79,7 @@ const makeEmployees = () => {
             },
             {
               type: "input",
-              name: "gitHub",
+              name: "github",
               message: "What is your Engineer's GitHub?",
             },
           ])
@@ -93,12 +88,12 @@ const makeEmployees = () => {
               engineerAnswers.name,
               engineerAnswers.id,
               engineerAnswers.email,
-              engineerAnswers.gitHub
+              engineerAnswers.github
             );
             employees.push(engineer);
             makeEmployees();
           });
-      } else if (typeAnswers === "Intern") {
+      } else if (typeAnswers.employee === "Intern") {
         //   inquirer prompt #4 Intern
         inquirer
           .prompt([
@@ -144,23 +139,3 @@ const makeEmployees = () => {
       }
     });
 };
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Template_Engine-employee-summary
+# Template Engine - Employee Summary
+  
+  ![badge](https://img.shields.io/badge/License-MIT-blue)
+
+  ## Description 
+  
+  For this project we were asked to build a software engineering team command line application.  This application will prompt the user for information about the team manager and then information about the team members.  The user can input any number of team members, and they can be any mix of engineers and interns.  We also utilize tests the check to make sure the code is working properly.
+
+  ## Table of Contents
+
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ##  Installation
+
+  make sure you have npm installed, and run app.js in the command line.
+
+  ## Usage
+
+  Once you run app.js in the command line the application will ask you a series of questions, starting with information about your manager, followed by any number of engineers and interns you want to add.  Once you have added all the employees you wish to add, select "no more employees" and it will render your information in the output folder as an html file.
+
+  ![ScreeenShot](./develop/screenshot.png)
+
+  ## License
+
+  This project is licensed under the MIT license.
+  
+
+  ## Tests
+
+  In the test folder we have a series of tests for each class of employee to verify they are working.
+
+  ## Questions
+
+  If you have any questions you can check out my github account: [Rseckman](https://github.com/Rseckman)
+  or contact me at rseckman@hotmail.com
